a4d9f30829ad7e70bbc23baccdb69c964531a5ad
Witally Bitaraes <witally_bitaraes@hotmail.com>
2021-01-04 21:56:37 -0300

    Estilizacao do Drawer- 2-Criacao estado User- 3. Utilizacao Styled Components

--- mobile/src/components/CustomDrawer/index.js
+++ mobile/src/components/CustomDrawer/index.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import { View, Text, Image} from 'react-native';
 import { DrawerNavigatorItems } from 'react-navigation-drawer';
 
+import {
+  Container,
+  UserArea,
+  UserName,
+  UserEmail,
+  Avatar
+} from "./styles";
 
 function CustomDrawer({...props}) {
+    const [user, setUser] = useState([
+      {
+        id: "1",
+        avatar: require("../../assets/user.png"),
+        name: "Witally Lana Bitarães",
+        email: "witally_bitaraes@hotmail.com"
+      }
+    ]);
+
    return(
-      <View>
-        <DrawerNavigatorItems {...props} /> 
-      </View>
+      <Container render>
+        
+        <UserArea>
+           <Avatar source={require("../../assets/user.png")} />
+           <UserName>Witally Lana Bitarães</UserName>
+           <UserEmail>witally_bitaraes@hotmail.com</UserEmail>
+         </UserArea>
+         
+         <DrawerNavigatorItems {...props} /> 
+      </Container>
    );
 } 
 
---
+++ mobile/src/components/CustomDrawer/styles.js
@@ -0,0 +1,30 @@
+import styled from "styled-components/native";
+
+
+export const Container = styled.SafeAreaView`
+  flex: 1;
+`;
+
+export const UserArea = styled.View`
+  margin-top: 15px;
+  margin-left: 10px;
+  margin-bottom: 15px;
+`;
+
+export const Avatar = styled.Image`
+  width: 70px;
+  height: 70px;
+`;
+
+export const UserName = styled.Text`
+  font-size: 18px;
+  margin-top: 5px;
+  color: #000;
+`;
+
+export const UserEmail = styled.Text`
+  font-size: 15px;
+  color: #484848;
+  margin-top: 5px;
+
+`;
